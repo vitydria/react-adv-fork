865962e4e75d25a53add2fd67b72c6e7c486b5ec
Victor Colmenares <colmenaresvictor2797@gmail.com>
2021-11-28 20:17:19 -0400

    Rutas anidadas listas

---
+++ src/lazyload/pages/NoLazy.tsx
@@ -0,0 +1,9 @@
+export const NoLazy = () => {
+  return (
+    <h1>
+      <h1>No LazyLoad Component</h1>
+    </h1>
+  );
+};
+
+export default NoLazy;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -12,19 +12,21 @@ interface Route {
   name: string;
 }
 //lazy metodo de React que nos permite cargar el componente
-const Lazy1 = lazy(
-  () => import(/*webpackChunkName: "LazyPage1"*/ "../lazyload/pages/LazyPage1")
+const LazyLayout = lazy(
+  () =>
+    import(/*webpackChunkName: "LazyLayout"*/ "../lazyload/layout/LazyLayout")
 );
-const Lazy2 = lazy(
-  () => import(/*webpackChunkName: "LazyPage2"*/ "../lazyload/pages/LazyPage2")
-);
-const Lazy3 = lazy(
-  () => import(/*webpackChunkName: "LazyPage3"*/ "../lazyload/pages/LazyPage3")
+const NoLazy = lazy(
+  () => import(/*webpackChunkName: "No Lazy"*/ "../lazyload/pages/NoLazy")
 );
 
 export const routes: Route[] = [
   //indicar el tipado y que es un array con []
-  { to: "/lazy1", path: "lazy1", Component: Lazy1, name: "Lazy-1" },
-  { to: "/lazy2", path: "lazy2", Component: Lazy2, name: "Lazy-2" },
-  { to: "/lazy3", path: "lazy3", Component: Lazy3, name: "Lazy-3" },
+  {
+    to: "/lazyload",
+    path: "lazyload/*",
+    Component: LazyLayout,
+    name: "LazyLayout",
+  },
+  { to: "/no-lazy", path: "no-lazy", Component: NoLazy, name: "No Lazy" },
 ];
